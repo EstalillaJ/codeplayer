4e13432b00a5b5468db3422e36b7c144c6d2718d
EstalillaJ <joshtestalilla@gmail.com>
2015-06-26 18:53:21 -0700

    Added hide functionality for text areas

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
 var height=$( window ).height()-40;
-
 $(".codecontainer").css("height",height+"px");
+
+$("#csscontainer").css("display","none");
+$("#jscontainer").css("display","none");
+
+$(".menu-item").click(function() {
+  $(this).toggleClass("selected");
+
+  var id= $(this).attr('name');
+ $("#"+id+"container").toggle();
+
+
+  var numDisplayed = $('.codecontainer').filter(function(){
+    return $(this).css('display') !== 'none';
+  }).length;
+
+
+  var width = 100/number;
+  $(".codecontainer").css("width", width+"%");
+});
